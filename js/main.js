2f99b2eae9a31fcd68cecd3fa9bd87ca1d29d3b6
David-Del1 <daviddel.731@gmail.com>
2021-01-14 13:26:19 -0800

    Finished product

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let menuBtn = document.querySelector('.menu-btn');
+
+let menuOpen = false;
+let nav = document.querySelector('nav')
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        nav.classList.add('active');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        nav.classList.remove('active');
+        menuOpen = false;
+    }
+    
+});
+
+$('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    })
+
+
+
+/*
+let toggle = document.getElementsByClassName('menu-btn')
+let nav = document.getElementsByClassName('nav')
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active')
+});
+*/
